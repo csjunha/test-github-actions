74f809f5cf981cf50bce4d740253dd9177023397
Junha Park <csjunha@greyboxhq.com>
2024-01-31 00:02:39 +0900

    test

--- source/lambda/test2/index.ts
+++ source/lambda/test2/index.ts
@@ -1,2 +1,3 @@
 console.log("Hello from test2");
 console.log("Hello from test2");
+console.log("Hello from test2");
--- source/lambda/test4/index.ts
+++ source/lambda/test4/index.ts
@@ -1,1 +1,2 @@
 console.log("Hello from test4");
+console.log("Hello from test4");
